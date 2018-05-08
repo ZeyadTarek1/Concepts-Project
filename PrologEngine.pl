c6720491ef71a444d12fd4ddda7b8780bcb85b35
Ziad <zeyad.tarek@msa.edu.eg>
2018-05-08 13:32:14 +0200

    Prolog Engine
    Added course database. Pre-reqs left.

---
+++ PrologEngine.pl
@@ -0,0 +1,162 @@
+/* Prolog Course Advisor expert search engine for Computer Science department */
+
+/* Database Construction */
+
+
+
+/* Year 1: General Year */
+
+/* Semester 1 */
+course(mth100).
+course(eng101x).
+course(cs100x).
+course(cs101x).
+course(mth103).
+course(hele).
+
+/* Semester 2 */
+course(cs102x).
+course(eng102x).
+course(mth106).
+course(phy103x).
+course(hele).
+
+
+/* Year 2: General Year */
+
+/* Semester 3 */
+course(eng201x).
+course(mth204).
+course(cs13).
+course(cs205).
+course(cs203).
+course(cs283).
+
+/* Semester 4 */
+course(cs217).
+course(cs232).
+course(cs215).
+course(cs216).
+course(cs214).
+course(mgt200).
+
+
+/* Year 3: Path 1: Computer Science */
+
+/* Semester 5 */
+course(cs351).
+course(cs334).
+course(cs314).
+course(cs316).
+course(fele).
+
+/* Semester 6 */
+course(cs326).
+course(cs381).
+course(cs352).
+course(cs364).
+course(fele).
+course(csele).
+
+
+/* Year 4: Path 1: Computer Science */
+
+/* Semester 7 */
+course(cs403).
+course(cs486).
+course(cs427).
+course(csele).
+course(fele).
+course(cs405x).
+
+/* Semester 8 */
+course(cs301).
+course(cs411).
+course(cs475).
+course(cs401).
+course(cs484).
+course(cs406).
+
+/* End of Computer Science Path */
+
+
+
+/* Year 3: Path 2: Internet Computing */
+
+/* Semester 5 */
+course(cs351).
+course(cs334).
+course(cs314).
+course(cs316).
+course(fele).
+
+/* Semester 6 */
+course(cs382).
+course(cs385).
+course(cs384).
+course(cs364).
+course(fele).
+course(csele).
+
+
+/* Year 4: Path 2: Internet Computing */
+
+/* Semester 7 */
+course(cs425).
+course(cs476).
+course(cs465).
+course(csele).
+course(fele).
+course(cs405x).
+
+/* Semester 8 */
+course(cs301).
+course(cs489).
+course(cs475).
+course(cs401).
+course(cs484).
+course(cs406).
+
+/* End of Internet Computing Path */
+
+
+
+/* Year 3: Path 3: Software Engineering */
+
+/* Semester 5 */
+course(cs351).
+course(cs334).
+course(cs314).
+course(cs316).
+course(fele).
+
+/* Semester 6 */
+course(cs347).
+course(cs344).
+course(cs384).
+course(cs364).
+course(fele).
+course(csele).
+
+
+/* Year 4: Path 3: Software Engineering */
+
+/* Semester 7 */
+course(cs425).
+course(cs442).
+course(cs465).
+course(csele).
+course(fele).
+course(cs405x).
+
+/* Semester 8 */
+course(cs301).
+course(cs458).
+course(cs475).
+course(cs401).
+course(cs484).
+course(cs406).
+
+/* End of Software Engineering Path */
+
+/* End of Database */
