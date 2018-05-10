0e7020258c8fc65964928f4903706db3afe41464
Ziad <zeyad.tarek@msa.edu.eg>
2018-05-10 16:57:27 +0200

    Added Student Samples + Admin Samples

---
+++ CSE_Student_Courses_Taken.pl
@@ -0,0 +1,47 @@
+
+/* Semester 1 */
+course(mat151).
+course(bsc152).
+course(gse153).
+course(gse154).
+course(com155).
+course(eng156).
+
+
+/* Semester 2 */
+course(mat161).
+course(bsc162).
+course(gse163).
+course(bsc164).
+course(gse165).
+course(eng166).
+
+
+/* Year 2 */
+
+/* Semester 3 */
+course(mat251).
+course(bsc252).
+course(ese253).
+course(ece254).
+course(com255).
+course(eng256).
+
+/* Semester 4 */
+course(mat261).
+course(ese262).
+course(ese263).
+course(ece264).
+course(com265).
+course(soc266).
+
+
+/* Year 3: */
+
+/* Semester 5 */
+course(mat351).
+course(cse352).
+course(ece353).
+course(cse354).
+course(ece355).
+course(ece356).
---
+++ CS_Student_Courses_Taken.pl
@@ -0,0 +1,66 @@
+
+/* Year 1: General Year */
+
+/* Semester 1 */
+course(mth100).
+course(eng101x).
+course(cs100x).
+course(cs101x).
+course(mth103).
+course(hele1).
+
+/* Semester 2 */
+course(cs102x).
+course(eng102x).
+course(mth106).
+course(phy103x).
+course(hele2).
+
+
+/* Year 2: General Year */
+
+/* Semester 3 */
+course(eng201x).
+course(mth204).
+course(cs13).
+course(cs205).
+course(cs203).
+course(cs283).
+
+/* Semester 4 */
+course(cs217).
+course(cs232).
+course(cs215).
+course(cs216).
+course(cs214).
+course(mgt200).
+
+
+/* Year 3: Path 1: Computer Science */
+
+/* Semester 5 */
+course(cs351).
+course(cs334).
+course(cs314).
+course(cs313).
+course(cs316).
+course(fele1).
+
+/* Semester 6 */
+course(cs326).
+course(cs381).
+course(cs352).
+course(cs364).
+course(fele2).
+course(csele1).
+
+
+/* Year 4: Path 1: Computer Science */
+
+/* Semester 7 */
+course(cs403).
+course(cs486).
+course(cs427).
+course(csele2).
+course(fele3).
+course(cs405x).
